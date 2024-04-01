35241945de8270fb3af738ba8fab5b02853a22ab
Abhimanyu kumar <kumarabhimanyu630@gmail.com>
2024-04-01 13:32:53 +0530

    complete user image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-blog-97329.firebaseapp.com",
